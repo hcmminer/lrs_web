2bf7bb3dffbcedf1aab90ba7556fea1c9e82ffb5
Bravi Dang <hcmminer@gmail.com>
2023-07-04 17:31:52 +0700

    change routing

--- ems/src/app/pages/config-system/config-system-routing.module.ts
+++ ems/src/app/pages/config-system/config-system-routing.module.ts
@@ -1,16 +1,15 @@
-
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { ConfigSystemComponent } from './config-system.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ConfigSystemComponent } from "./config-system.component";
 
 const routes: Routes = [
   {
-    path: '',
+    path: "",
     // component: QuanLyDanhMucComponent,
     children: [
-      { path: 'search', component: ConfigSystemComponent},
-      { path: '', redirectTo: 'search', pathMatch: 'full' },
-      { path: '**', redirectTo: 'error/404', pathMatch: 'full' },
+      { path: "options", component: ConfigSystemComponent },
+      { path: "", redirectTo: "search", pathMatch: "full" },
+      { path: "**", redirectTo: "error/404", pathMatch: "full" },
     ],
   },
 ];
